1fe7980f6e29ffa199dba3bea2527a012e9cbfd4
pratik1803 <pratikvaidya1803@gmail.com>
2023-01-18 19:53:19 +0530

    Initialized project with basic gql server

---
+++ data.js
@@ -0,0 +1,32 @@
+const UserList = [
+  {
+    id: 1,
+    name: "Pedro1",
+    username: "PedroTech1",
+    age: 20,
+    nationality: "BRAZIL1",
+  },
+  {
+    id: 2,
+    name: "Pedro2",
+    username: "PedroTech2",
+    age: 20,
+    nationality: "BRAZIL2",
+  },
+  {
+    id: 3,
+    name: "Pedro3",
+    username: "PedroTech3",
+    age: 20,
+    nationality: "BRAZIL3",
+  },
+  {
+    id: 4,
+    name: "Pedro4",
+    username: "PedroTech4",
+    age: 20,
+    nationality: "BRAZIL4",
+  },
+];
+
+module.exports = { UserList };
---
+++ index.js
@@ -0,0 +1,12 @@
+const { ApolloServer } = require("apollo-server");
+const { typeDefs } = require("./schema/typedefs");
+const { resolvers } = require("./schema/resolver");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then((url) => {
+  console.log(`Your Api is Runningg ;) At ${url.url}`);
+});
